38c83d71a346d9e733ea3c691c2d37cf46530f5a
llf137224350 <137224350@qq.com>
2022-04-13 15:31:27 +0800

    feat: 增加回车搜索

--- js/index.js
+++ js/index.js
@@ -2,10 +2,23 @@
   var date = new Date();
   document.getElementById("copyright").innerText = "©" + date.getFullYear();
   // 搜索
-  document.getElementById("search").addEventListener("click", function () {
-    var value = document.getElementById("input").value.trim();
+  function search() {
+    var input = document.getElementById("input");
+    var value = input.value.trim();
     if (value) {
+      input.value = "";
       window.open("https://www.baidu.com/s?wd=" + encodeURIComponent(value));
+    }
+  }
+  // 点击搜索
+  document.getElementById("search").addEventListener("click", search);
+  // 回车
+  document.getElementById("input").addEventListener("keydown", function (e) {
+    var theEvent = e || window.event;
+    var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+    if (code == 13) {
+      //回车执行查询
+      search();
     }
   });
 })();
